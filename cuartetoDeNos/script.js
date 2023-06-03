a7f22ee19ee27c0275cb81ce707bf909b6c0878e
abrahamalanya <alanyaoficial@gmail.com>
2023-06-03 23:51:28 +0200

    cuartetoDeNos

---
+++ cuartetoDeNos/script.js
@@ -0,0 +1,185 @@
+const data = [
+  {
+    name: 'inicio',
+    title: 'El Cuarteto de Nos',
+    description: `El Cuarteto de Nos es una banda uruguaya que ha dejado una huella significativa en el rock de su país. 🎸 Desde su nacimiento en los años 80 hasta consolidarse en los 90, se han destacado por su irreverencia, humor negro y su estilo musical diverso. 
+    <br><br>🤘 Su álbum "Otra Navidad en las Trincheras" se convirtió en el disco más vendido en la historia del rock uruguayo, y desde entonces han lanzado 16 trabajos discográficos que les han valido numerosos reconocimientos. 🎶 En el 2006, sorprendieron al mundo con "Raro", un álbum que marcó su regreso masivo y su internacionalización. 🌎 Además, han colaborado con el reconocido músico y productor Juan Campodónico, quien les ha ayudado a renovar su sonido y destacar sus letras filosas e inteligentes.
+    <br><br>🎵 Su éxito ha trascendido fronteras, siendo nominados a los Premios Latin Grammy y consolidándose en países como Argentina, Colombia, México y España. 🏆 Su último disco, "Jueves", fue producido por cuatro talentosos productores, reflejando la diversidad estilística de la banda. 🎧 Con canciones como "Contrapunto para humano y computadora" y "Punta Cana", han demostrado su calidad artística y su capacidad para generar reflexiones críticas.
+    <br><br>💡 Recientemente, lanzaron el álbum "Lámina Once", en el cual exploran la vida actual y plasman preguntas filosóficas a través de nueve imágenes inspiradas en los Test de Rorschach. 🖼️ Con su sello propio, Porfiado Records, El Cuarteto de Nos sigue dejando su marca en la escena musical y cautivando a sus seguidores con su estilo único y provocador. 👏`
+  },
+  {
+    name: 'integrantes',
+    title: 'integrantes'
+  },
+  {
+    name: 'discografia',
+    title: 'discografía'
+  },
+  {
+    name: 'contacto',
+    title: 'contacto',
+    content: `
+          <a href="https://open.spotify.com/artist/13JJKrUewC1CJYmIDXQNoH" target="blank">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.001 2C6.50098 2 2.00098 6.5 2.00098 12C2.00098 17.5 6.50098 22 12.001 22C17.501 22 22.001 17.5 22.001 12C22.001 6.5 17.551 2 12.001 2ZM15.751 16.65C13.401 15.2 10.451 14.8992 6.95014 15.6992C6.60181 15.8008 6.30098 15.55 6.20098 15.25C6.10098 14.8992 6.35098 14.6 6.65098 14.5C10.451 13.6492 13.751 14 16.351 15.6C16.701 15.75 16.7501 16.1492 16.6018 16.45C16.4018 16.7492 16.0518 16.85 15.751 16.65ZM16.7501 13.95C14.051 12.3 9.95098 11.8 6.80098 12.8C6.40181 12.9 5.95098 12.7 5.85098 12.3C5.75098 11.9 5.95098 11.4492 6.35098 11.3492C10.001 10.25 14.501 10.8008 17.601 12.7C17.9018 12.8508 18.051 13.35 17.8018 13.7C17.551 14.05 17.101 14.2 16.7501 13.95ZM6.30098 9.75083C5.80098 9.9 5.30098 9.6 5.15098 9.15C5.00098 8.64917 5.30098 8.15 5.75098 7.99917C9.30098 6.94917 15.151 7.14917 18.8518 9.35C19.301 9.6 19.451 10.2 19.201 10.65C18.9518 11.0008 18.351 11.1492 17.9018 10.9C14.701 9 9.35098 8.8 6.30098 9.75083Z" fill="rgba(255,255,255,1)"></path></svg>
+          </a>
+          <a href="https://www.youtube.com/user/cuartetodenos" target="blank">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.2439 4C12.778 4.00294 14.1143 4.01586 15.5341 4.07273L16.0375 4.09468C17.467 4.16236 18.8953 4.27798 19.6037 4.4755C20.5486 4.74095 21.2913 5.5155 21.5423 6.49732C21.942 8.05641 21.992 11.0994 21.9982 11.8358L21.9991 11.9884L21.9991 11.9991C21.9991 11.9991 21.9991 12.0028 21.9991 12.0099L21.9982 12.1625C21.992 12.8989 21.942 15.9419 21.5423 17.501C21.2878 18.4864 20.5451 19.261 19.6037 19.5228C18.8953 19.7203 17.467 19.8359 16.0375 19.9036L15.5341 19.9255C14.1143 19.9824 12.778 19.9953 12.2439 19.9983L12.0095 19.9991L11.9991 19.9991C11.9991 19.9991 11.9956 19.9991 11.9887 19.9991L11.7545 19.9983C10.6241 19.9921 5.89772 19.941 4.39451 19.5228C3.4496 19.2573 2.70692 18.4828 2.45587 17.501C2.0562 15.9419 2.00624 12.8989 2 12.1625V11.8358C2.00624 11.0994 2.0562 8.05641 2.45587 6.49732C2.7104 5.51186 3.45308 4.73732 4.39451 4.4755C5.89772 4.05723 10.6241 4.00622 11.7545 4H12.2439ZM9.99911 8.49914V15.4991L15.9991 11.9991L9.99911 8.49914Z" fill="rgba(255,255,255,1)"></path></svg>
+          </a>
+          <a href="https://www.youtube.com/user/cuartetodenos" target="blank">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.001 2C6.47813 2 2.00098 6.47715 2.00098 12C2.00098 16.9913 5.65783 21.1283 10.4385 21.8785V14.8906H7.89941V12H10.4385V9.79688C10.4385 7.29063 11.9314 5.90625 14.2156 5.90625C15.3097 5.90625 16.4541 6.10156 16.4541 6.10156V8.5625H15.1931C13.9509 8.5625 13.5635 9.33334 13.5635 10.1242V12H16.3369L15.8936 14.8906H13.5635V21.8785C18.3441 21.1283 22.001 16.9913 22.001 12C22.001 6.47715 17.5238 2 12.001 2Z" fill="rgba(255,255,255,1)"></path></svg>
+          </a>`
+  }
+];
+
+// modal
+const modal = document.querySelector("#mainModal");
+function openModal() {
+  modal.classList.add('modal');
+  modal.showModal();
+}
+function closeModal() {
+  modal.classList.remove('modal');
+  modal.close();
+}
+
+// section
+function section(text) {
+  const container = document.querySelector("#sectionContainer");
+  let html, gallery, btn;
+  let section = data;
+
+  section.forEach(function (e) {
+    if (e.name == text) {
+      switch (e.name) {
+        case 'inicio':
+          btn = `<button id="openModal" class="btn">Biografía</button>`;
+          break;
+        
+        case 'integrantes':
+          gallery = `<article class="gallery">
+                      <div class="card">
+                        <img src="./img/robertoMusso.jpg" alt="robertoMusso">
+                        <figure>roberto Musso</figure>
+                      </div>
+                      <div class="card">
+                        <img src="./img/gustavoAntuna.jpg" alt="gustavoAntuna">
+                        <figure>gustavo Antuna</figure>
+                      </div>
+                      <div class="card">
+                        <img src="./img/santiagoMarrero.jpg" alt="santiagoMarrero">
+                        <figure>santiago Marrero</figure>
+                      </div>
+                      <div class="card">
+                        <img src="./img/santiagoTavela.jpg" alt="santiagoTavela">
+                        <figure>santiago Tavela</figure>
+                      </div>
+                      <div class="card">
+                        <img src="./img/alvaroPintos.jpg" alt="alvaroPintos">
+                        <figure>alvaro Pintos</figure>
+                      </div>
+                    </article>`;
+          break;
+        
+        case 'discografia':
+          gallery = `<article class="gallery">
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/laminaOnce.jpg" alt="laminaOnce">
+                        <figure style="width: 260px;">lámina one</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/jueves.jpg" alt="jueves">
+                        <figure style="width: 260px;">jueves</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/apocalipsisZombie.jpg" alt="apocalipsisZombie">
+                        <figure style="width: 260px;">apocalipsis zombie</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/hablaTuEspejo.jpg" alt="hablaTuEspejo">
+                        <figure style="width: 260px;">habla tu espejo</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/porfiado.jpg" alt="porfiado">
+                        <figure style="width: 260px;">porfiado</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/bipolar.jpg" alt="bipolar">
+                        <figure style="width: 260px;">bipolar</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/raro.jpg" alt="raro">
+                        <figure style="width: 260px;">raro</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/elCuartetoDeNos.jpg" alt="elCuartetoDeNos">
+                        <figure style="width: 260px;">el Cuarteto De Nos</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/cortamambo.jpg" alt="cortamambo">
+                        <figure style="width: 260px;">cortamambo</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/revistaEsta.jpg" alt="revistaEsta">
+                        <figure style="width: 260px;">revista esta</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/elTrenBala.jpg" alt="elTrenBala">
+                        <figure style="width: 260px;">el tren bala</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/barrancaAbajo.jpg" alt="barrancaAbajo">
+                        <figure style="width: 260px;">barranca abajo</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/otraNavidadEnLasTrincheras.jpg" alt="otraNavidadEnLasTrincheras">
+                        <figure style="width: 260px;">otra navidad en las trincheras</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/cancionesDelCorazon.jpg" alt="cancionesDelCorazon">
+                        <figure style="width: 260px;">canciones del corazón</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/emilioGarcia.jpg" alt="emilioGarcia">
+                        <figure style="width: 260px;">emilio garcia</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/soyUnaArveja.jpg" alt="soyUnaArveja">
+                        <figure style="width: 260px;">soy una arveja</figure>
+                      </div>
+                      <div class="card">
+                        <img style="width: 260px;" src="./img/albertoWolfYElCuartetoDeNos.jpg" alt="albertoWolfYElCuartetoDeNos">
+                        <figure style="width: 260px;">alberto wolf y el cuarteto de nos</figure>
+                      </div>
+                    </article>`;
+          break;
+        
+        case 'contacto':
+          break;
+
+        default:
+          break;
+      }
+
+      html = `
+        <h1>${e.title}</h1>
+        <div class="rrss">${e.content ? e.content : ''}</div>
+        ${gallery ? gallery : ''}
+        ${btn ? btn : ''}
+      `;
+      
+      container.innerHTML = html;
+      modal.innerHTML = `
+        <p>${e.description ? e.description : ''}</p>
+        <button id="closeModal" class="btn">Cerrar</button>
+      `;
+
+      if (btn) {
+        const openModalBtn = document.querySelector("#openModal");
+        const closeModalBtn = document.querySelector("#closeModal");
+        openModalBtn.addEventListener('click', openModal);
+        closeModalBtn.addEventListener('click', closeModal);  
+      }
+    }
+  })
+}
+section('inicio');
